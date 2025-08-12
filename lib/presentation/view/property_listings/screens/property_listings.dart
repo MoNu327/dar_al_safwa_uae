cc016eceebae3458a594041cb933f919509168d0
EmElBiNu631 <emelbinu883@gmail.com>
2025-08-12 12:35:26 +0530

    UAE CODES

--- lib/presentation/view/property_listings/screens/property_listings.dart
+++ lib/presentation/view/property_listings/screens/property_listings.dart
@@ -361,10 +361,10 @@ class PropertyListings extends StatelessWidget {
     final formattedPrice = isArabic
         ? (price?.formatted?.ar?.isNotEmpty ?? false
             ? price!.formatted!.ar
-            : '${price?.raw ?? 0} OMR')
+            : '${price?.raw ?? 0} AED')
         : (price?.formatted?.en?.isNotEmpty ?? false
             ? price!.formatted!.en
-            : '${price?.raw ?? 0} OMR');
+            : '${price?.raw ?? 0} AED');
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
